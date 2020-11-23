8c1daba72dbc79227865e8b68c305f8c0e47bc0f
xrkffgg <xrkffgg@vip.qq.com>
2020-11-23 14:51:00 +0800

    ci: fix unused

--- script/check-application.js
+++ script/check-application.js
@@ -69,7 +69,6 @@ async function main() {
   const emailExp = new RegExp('^([a-zA-Z0-9]+[_|\\_|\\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\\_|\\.]?)*[a-zA-Z0-9]+\\.[a-zA-Z]{2,3}$', '');
 
   if (!email || !emailExp.test(email) || (userNowApp === 0)) {
-    core.info('empty');
     await octokit.issues.createComment({
       owner,
       repo,
